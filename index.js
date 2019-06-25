849bcfab8153d772d32643e7c93b98229baf835b
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2019-06-24 21:21:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -119,4 +119,15 @@ client.on('guildMemberRemove', member => {
   channel.send(`Bye ${member}, Hope to see you again!`);
 });
 
+// Output audit log entries
+guild.fetchAuditLogs()
+  const loggingch = member.guild.channels.find(ch => ch.name ==='admin-logs-and-bot-logs')
+  .then(audit => loggingch(audit.entries.first()))
+  .catch(console.error);
+
+// Get ban
+guild.fetchBan(message.author)
+  .then(({ user, reason }) => console.log(`${user.tag} was banned for the reason: ${reason}.`))
+  .catch(console.error);
+
 client.login(process.env.BOT_TOKEN);
