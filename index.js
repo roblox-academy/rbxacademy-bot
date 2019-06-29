ffce7abf9eb9f1b091132959b667068cf78e97df
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2019-06-28 21:19:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('Roblox Academy', { type: 'PLAYING' });
 });
 
 client.on('message', msg => {
