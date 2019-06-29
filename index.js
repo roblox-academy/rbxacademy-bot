5c160404cfcc0a5dd2308db6da117f48577bfd3b
joshuadl12 <joshuadeleon210@gmaul.com>
2019-06-29 18:35:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setActivity('https://www.robloxacademy.tk', { type: 'PLAYING' });
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
